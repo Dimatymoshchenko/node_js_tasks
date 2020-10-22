0e042d596ad844fc25daca097b0684586c814c50
dtymoshchenko <dtymoshchenko@lohika.com>
2020-10-22 11:59:18 +0300

    - made server - add logger - made config - configure README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Node.js Homework
+#Node.js Homework
+
+###To run app:
+```
+$ PORT=8080 node app.js -env='prod'
+```
+
+###To check app:
+```
+$ curl localhost:8080
+```
---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const { PORT, ENV } = require('./config');
+const { log, debug, warn, error } = require('./logger');
+
+http.createServer((req, res) => {
+  res.writeHeader(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Hello world!' }));
+}).listen(PORT, () => log(`Listening on port ${PORT}...`));
